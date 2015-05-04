282d7fb1a27afe96b605e3f160aca41063f7eac6
ivermac <mark.ekisa@gmail.com>
2015-05-04 14:31:57 +0300

    ME: added helper method 'get_email_and_token'

--- dkobo/koboform/utils.py
+++ dkobo/koboform/utils.py
@@ -1,5 +1,9 @@
+import jwt
 import hashlib
 import urllib
+
+from django.conf import settings
+
 
 def create_survey_from_csv_text(csv_text):
     # I suspect that pyxform is the reason that the server might be running slowly
@@ -7,8 +11,23 @@ def create_survey_from_csv_text(csv_text):
     import pyxform_utils
     return pyxform_utils.create_survey_from_csv_text(csv_text)
 
+
 def gravatar_url(email):
     gravatar_url = "http://www.gravatar.com/avatar/"
     gravatar_url += hashlib.md5(email.lower()).hexdigest() + "?"
     gravatar_url += urllib.urlencode({'s': '40'})
     return gravatar_url
+
+
+def get_email_and_token(request):
+    ona_user_cookie = request.COOKIES.get('ona_user')
+    email = ''
+    token = ''
+    if ona_user_cookie:
+        token_payload = jwt.decode(ona_user_cookie,
+                                   settings.JWT_SECRET_KEY,
+                                   algorithms=[settings.JWT_ALGORITHM])
+        email = token_payload.get('email')
+        token = token_payload.get('token')
+
+    return (email, token)
